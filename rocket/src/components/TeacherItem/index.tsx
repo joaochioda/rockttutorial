666a72a38f7e86dec999479bc4a4918b7b514406
joaochioda <joao.chioda@dextra-sw.com>
2020-08-07 22:12:30 -0300

    criando conexao ao clicar no botao do zap

--- rocket/src/components/TeacherItem/index.tsx
+++ rocket/src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -16,6 +17,12 @@ interface TeacherItemProps {
   teacher: Teacher
 }
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  };
   return (
     <article className="teacher-item">
       <header>
@@ -39,7 +46,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
                 <strong>R$ {teacher.cost}</strong>
         </p>
-          <a href={`https://wa.me/${teacher.whatsapp}`}>
+          <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
                 Entrar em contato
             </a>
